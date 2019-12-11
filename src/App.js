96edf903869a2ef163318a39c17d28e5cbe8d0be
Ayush <ayush.s@pharmeasy.in>
2019-12-11 19:14:26 +0530

    Search and sorting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import api from './api';
 
 import Rating from './Rating';
+import Loader from './Loader';
 
 import { ReactComponent as Logo } from './logo.svg';
 import './App.scss';
@@ -10,30 +11,93 @@ import './App.scss';
 class App extends React.Component {
   state = {
     movies: [],
+    loading: false,
+    page: 0,
+    query: '',
   };
 
   componentDidMount() {
-    api.get('popular').then(res => {
-      const { results, page } = res.data;
-      this.setState({ movies: results, page });
-    });
+    this.loadMovies();
   }
 
+  loadMovies = () => {
+    this.setState({ loading: true });
+    api
+      .get('/movie/popular', {
+        params: {
+          page: this.state.page + 1,
+        },
+      })
+      .then(res => {
+        const { results, page } = res.data;
+        this.setState(
+          {
+            movies: [...this.state.movies, ...results],
+            initialMovies: [...this.state.movies, ...results],
+            page,
+            loading: false,
+          },
+          this.sort
+        );
+      });
+  };
+
+  handleSearch = event => {
+    this.setState({ query: event.target.value }, () => {
+      const { query } = this.state;
+      if (query.length > 1) {
+        api
+          .get('/search/movie', {
+            params: {
+              query,
+            },
+          })
+          .then(res => {
+            const { results } = res.data;
+            this.setState({ movies: results, initialMovies: results, loading: false }, this.sort);
+          });
+      }
+    });
+  };
+
+  sort = () => {
+    const { sort, movies, initialMovies } = this.state;
+    const MOVIES = movies;
+    if (sort === 'clear') {
+      this.setState({ movies: initialMovies });
+    } else {
+      this.setState({ movies: MOVIES.sort((a, b) => (a[sort] < b[sort] ? 1 : -1)) });
+    }
+  };
+
+  handleSort = event => {
+    console.log(event.target.value);
+    this.setState({ sort: event.target.value }, this.sort);
+  };
+
+  clearSearch = () => {
+    this.setState({ movies: [], query: '', page: 0 }, this.loadMovies);
+  };
+
   render() {
+    const { loading, movies, query, sortValue } = this.state;
     return (
       <div className="app">
         <header className="header">
           <Logo width="80" />
           <h1 className="title">Movie Library</h1>
         </header>
-        <form id="search" className="search-container">
-          <input type="text" className="search" placeholder="Search" />
-          <button type="submit" className="submit">
-            <i className="fas fa-search" />
-          </button>
-        </form>
+        <div className="search-container">
+          <input type="text" className="search" placeholder="Search" value={query} onChange={this.handleSearch} />
+          {query && <button className="clear fas fa-times" onClick={this.clearSearch} />}
+          <select value={sortValue} className="sort" onChange={this.handleSort}>
+            <option value="clear">Sort By</option>
+            <option value="vote_average">Rating</option>
+            <option value="release_date">Year</option>
+          </select>
+        </div>
         <div className="grid">
-          {this.state.movies.map(movie => (
+          {movies.map(movie => (
             <div className="movie-card" key={movie.id}>
               <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt="" className="movie-poster" />
               <h4 className="movie-title">{movie.title}</h4>
@@ -41,6 +105,13 @@ class App extends React.Component {
             </div>
           ))}
         </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <button className="load-more" onClick={this.loadMovies}>
+            Load More
+          </button>
+        )}
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export default axios.create({
   baseURL: BASE_URL,
