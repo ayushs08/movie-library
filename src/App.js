094681dec8afc57045874ca615e808f7e59b6499
Ayush <ayush.s@pharmeasy.in>
2019-12-11 11:57:03 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import stickybits from 'stickybits';
+
+import api from './api';
+
+import Rating from './Rating';
 
 import { ReactComponent as Logo } from './logo.svg';
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    movies: [],
+  };
+
   componentDidMount() {
-    stickybits('#search', { useStickyClasses: true });
+    api.get('popular').then(res => {
+      const { results, page } = res.data;
+      this.setState({ movies: results, page });
+    });
   }
 
   render() {
@@ -22,6 +32,15 @@ class App extends React.Component {
             <i className="fas fa-search" />
           </button>
         </form>
+        <div className="grid">
+          {this.state.movies.map(movie => (
+            <div className="movie-card" key={movie.id}>
+              <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt="" className="movie-poster" />
+              <h4 className="movie-title">{movie.title}</h4>
+              <Rating rating={movie.vote_average} defaultBase={10} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
