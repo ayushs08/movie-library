25407045d6e1596d716744ad1303f19038208a1f
Ayush <ayush.s@pharmeasy.in>
2019-12-16 10:00:24 +0530

    Updates

---
+++ precache-manifest.b2f562d1cd8da59ccb14a5547445f737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63faa9e005df8335f50c4c9ed03b873",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "9c793dea903bdcb8b31a",
+    "url": "/movie-library/static/css/main.f0dcbf53.chunk.css"
+  },
+  {
+    "revision": "0b5b9c11ca0bd0a9f689",
+    "url": "/movie-library/static/js/2.68698af5.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie-library/static/js/2.68698af5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c793dea903bdcb8b31a",
+    "url": "/movie-library/static/js/main.5a6a474e.chunk.js"
+  },
+  {
+    "revision": "93951ac7aeaf6d103eef",
+    "url": "/movie-library/static/js/runtime-main.23608902.js"
+  },
+  {
+    "revision": "be368cc8a360236f4ec7382c9a5ce81f",
+    "url": "/movie-library/static/media/logo.be368cc8.svg"
+  }
+]);
